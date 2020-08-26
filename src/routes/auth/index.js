8073a94ff3425727442ec2ef3d966a1cd82674d4
tetiana <tetianayaremko@gmail.com>
2020-08-26 17:07:51 +0200

    refreshToken

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -40,4 +40,38 @@ router.route("/login").post(async (req, res, next) => {
   );
 });
 
+router.route("/token").post(async (req, res, next) => {
+  const refreshToken = req.body.refreshToken;
+  if (refreshToken) {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_JWT_KEY);
+    const user = await User.findOne({ where: { _id: decoded._id } });
+    if (!user) res.status(401);
+    else {
+      const currentToken = user.refresh_tokens.find(
+        (token) => token === refreshToken
+      );
+      if (!currentToken) res.status(401);
+      else {
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY, {
+          expiresIn: 300,
+        });
+        const refreshToken = jwt.sign(
+          { _id: user._id },
+          process.env.REFRESH_JWT_KEY,
+          { expiresIn: "1 week" }
+        );
+        user.refresh_tokens = user.refresh_tokens
+          .filter((token) => token !== currentToken)
+          .concat(refreshToken);
+        await User.update(
+          { refresh_tokens: user.refresh_tokens },
+          { where: { _id: user._id } }
+        );
+        res.send({ token, refreshToken });
+      }
+    }
+  } else {
+    res.status(401);
+  }
+});
 module.exports = router;
